fcdb8ac5e83a80786febc0fee9eb300fc253095e
Dulaj Hansana <dulajhansana555@gmail.com>
2025-08-29 08:05:47 +0000

    still the same

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -40,7 +40,7 @@ export function Header() {
       </div>
 
       {/* Main Navigation */}
-      <div className="container mx-auto flex items-center justify-between px-4 py-3">
+      <div className="container mx-auto flex items-center justify-between px-4 py-2">
         <div className="flex items-center">
           <Logo />
         </div>
